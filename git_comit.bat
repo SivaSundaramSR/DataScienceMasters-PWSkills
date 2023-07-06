878bad90c4d65c6bc27629afef83526096f7ee05
Siva Sundaram S R <srss.siva@hotmail.com>
2023-07-06 16:44:31 +0530

    Datascience commit

---
+++ git_comit.bat
@@ -0,0 +1,10 @@
+git init
+git add README.md
+git config --global user.email "srss.siva@hotmail.com"
+git config --global user.name "Siva Sundaram S R"
+git add .
+git commit -m "Datascience commit"
+git branch -M main
+git remote remove origin
+git remote add origin https://github.com/SivaSundaramSR/DataScienceMasters-PWSkills.git
+git push -u origin main
